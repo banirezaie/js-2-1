686c8ce9b0fb828d982a9ce5bd63b74566c6e57d
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-27 20:16:15 +0430

    object learned

--- objectsSyntax.js
+++ objectsSyntax.js
@@ -1,16 +1,16 @@
 let kitten = {
-    furColour: "orange",
-    age: "23"
-}
+	furColour: "orange",
+	age: "23",
+};
 
 let laptop = {
-    brand: "Lenovo",
-    ram: "5GB"
-}
+	brand: "Lenovo",
+	ram: "5GB",
+};
 
 let phone = {
-    operatingSystem: "iOS",
-    hasStylus: true,
-    megapixels: 12,
-    batteryLife: "24 hours"
-}
+	operatingSystem: "iOS",
+	hasStylus: true,
+	megapixels: 12,
+	batteryLife: "24 hours",
+};
