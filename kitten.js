b457b8815a3ac8094b3e58b30795dc6937f3b2b0
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-27 20:17:33 +0430

    kitten done

--- kitten.js
+++ kitten.js
@@ -1,16 +1,13 @@
-/*
-    Console.log the values of each property of "kitten"
-*/
+//TODO Console.log the values of each property of "kitten"
 
 let kitten = {
-  ageMonths: 3,
-  isFemale: true,
-  furColour: "brown",
-  true: "it's true",
-  5: "it's a number",
-}
+	ageMonths: 3,
+	isFemale: true,
+	furColour: "brown",
+};
 
-//bad practice
-console.log(kitten.ageMonths, kitten[("isFemale", "furColour")])
-//good practice
-console.log(kitten.ageMonths, kitten["isFemale"], kitten["fur"+"Colour"], kitten["furColour"])
+//! bad practice
+console.log(kitten.ageMonths, kitten[("isFemale", "furColour")]); // 3 brown
+
+//! good practice
+console.log(kitten.ageMonths, kitten["isFemale"], kitten["furColour"]); // 3 true brown brown
