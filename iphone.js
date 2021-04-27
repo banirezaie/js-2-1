4914a842a7ce76be4234a65a23c3f559b01a6b5f
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-27 20:11:07 +0430

    selecting property learned

--- iphone.js
+++ iphone.js
@@ -1,12 +1,11 @@
 let phone = {
-  brand: 'iPhone',
-  model: 'iPhone X',
-  launchYear: 2017,
-  isUnlocked: true
-}
+	brand: "iPhone",
+	model: "iPhone X",
+	launchYear: 2017,
+	isUnlocked: true,
+};
 
 let phoneBrand = phone.brand;
 let phoneLaunchYear = phone["launchYear"];
 
-console.log(phoneBrand, phoneLaunchYear)
-
+console.log(phoneBrand, phoneLaunchYear); // iPhone 2017
--- objects.js
+++ objects.js
@@ -1,34 +1,40 @@
-let laptop = {
-  brand: "Lenovo",
-  colors: ["spaceGray", "black", "white"],
-  screenSize: 13,
-  isTouchscreen: true,
+//TODO Uncomment each section and give reasons for the output
+
+/* let laptop = {
+	brand: "Lenovo",
+	screenSize: 13,
+	isTouchscreen: true,
+	colors: ["spaceGray", "black", "white"],
+};
+
+console.log(laptop); // the whole object
+console.log(laptop.brand); // Lenovo
+console.log(laptop["brand"]); // Lenovo
+ */
+
+/* -------------------------------------------------------------------------- */
+
+/* let animal = "Octopus";
+function observe() {
+	let animal = "Squid";
+	console.log(animal);
 }
+observe(); // Octopus
+ */
 
-// console.log(laptop.brand)
-// console.log(laptop.colors[1])
-// console.log(laptop["brand"])
-// console.log(laptop["colors"])
+/* const creature = "Dragon";
+function scubaDive() {
+	const creature = "Spanish Dancer"; //a type of sea slug
+	console.log(creature);
+}
+scubaDive(); // Spanish Dancer
+ */
 
-laptop.brand = "Apple"
-console.log(laptop)
-laptop.memory = 16;
-console.log(laptop)
-
-// let animal = "Octopus"
-// function observe() {
-//   let animal = "Squid"
-//   console.log(animal)
-// }
-
-// observe()
-
-// const creature = "Dragon"
-
-// function scubaDive() {
-//   const creature = "Spanish Dancer" //a type of sea slug
-//   console.log(creature)
-// }
-
-// scubaDive()
-
+/* let deadlyAnimal = "Blue-Ringed Octopus";
+function handleAnimal() {
+	let deadlyAnimal = "Scorpion fish";
+	console.log(deadlyAnimal);
+}
+handleAnimal(); // Scorpion fish
+console.log(deadlyAnimal); // Blue-Ringed Octopus
+ */
