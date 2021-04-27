4d7e3fb17e3e9e4acdfe65aaf4a10e28a3a88578
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-27 20:16:30 +0430

    method learned

--- method.js
+++ method.js
@@ -1,16 +1,14 @@
-//Math
-
 const myMath = {
-  PI: 3.14,
-  square: function (num) {
-    return num * num
-  },
-  cube: function (num) {
-    return num * num * num
-  },
-}
+	PI: 3.14,
+	square: function (num) {
+		return num * num;
+	},
+	cube(num) {
+		return num * num * num;
+	},
+};
 
-console.log(myMath.square(12))
-console.log(myMath.cube(12))
-console.log(myMath.PI)
-console.log(myMath.square(myMath.PI))
+console.log(myMath.square(12)); // 144
+console.log(myMath.cube(12)); // 1728
+console.log(myMath.PI); // 3.14
+console.log(myMath.square(myMath.PI)); // 9.8596
