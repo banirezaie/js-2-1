7b73875c8419abb0e65a8cb592c260fb19460b63
fatemeh.sargolzai <78509827+fatemeh79sargolzaii@users.noreply.github.com>
2021-04-28 23:03:29 +0430

    fatemehs

--- mozafar.js
+++ mozafar.js
@@ -13,9 +13,10 @@ let mentorsAges = {
 
 // ONLY EDIT BELOW THIS LINE
 
-let mentorsNames = ;
+let mentorsNames = Object.keys(mentorsAges) ;
 
-let mentorsNamedUppercased = ;
+let mentorsNamedUppercased = mentorsNames.map(name=>name.toUpperCase());
+
 
 // ONLY EDIT ABOVE THIS LINE
 
