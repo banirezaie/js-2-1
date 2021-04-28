590f265b6b9ea824af7329ab946ac3c76d59a61a
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-29 01:04:40 +0430

    warmup3 updated

--- warmup3.js
+++ warmup3.js
@@ -5,9 +5,7 @@ Each restaurant has a name, the number of total seats, the number of customers i
 We want to build an object to simulate a Restaurant Finder application (see below restaurantFinderApplication).
 
 1- Define a method findAvailableRestaurants which takes a number of people in parameter and returns all the restaurant names which have the required number of seats available at the moment.
-
 2- Define a method findRestaurantServingDish which takes a dish name in parameter and returns all the restaurant names serving this dish.
-
 3- Define a method countNumberOfRestaurantsInArea which takes an area of Glasgow in parameter (center, west), and returns the number of restaurants in this area.
 */
 
@@ -51,36 +49,33 @@ const restaurantFinderApplication = {
 	applicationVersion: "1.0",
 	restaurants: restaurants,
 	findAvailableRestaurants(numberOfPeople) {
-		this.restaurants.forEach((restaurant) => {
-			if (restaurant.totalSeats - restaurant.numberOfCustomers >= numberOfPeople) {
-				console.log(restaurant.name);
-			}
-		});
+		return this.restaurants
+			.filter((restaurant) => restaurant.totalSeats - restaurant.numberOfCustomers >= numberOfPeople)
+			.map((restaurant) => restaurant.name);
 	},
 	findRestaurantServingDish(dishName) {
-		this.restaurants.forEach((restaurant) => {
-			if (restaurant.menu.includes(dishName)) console.log(restaurant.name);
-		});
+		return this.restaurants
+			.filter((restaurant) => restaurant.menu.includes(dishName))
+			.map((restaurant) => restaurant.name);
 	},
 	countNumberOfRestaurantsInArea(area) {
-		console.log(this.restaurants.filter((restaurant) => restaurant.address.area === area).length);
+		return this.restaurants.filter((restaurant) => restaurant.address.area === area).length;
 	},
 };
 
-console.log("\nFind available restaurants for 5 people: ");
-restaurantFinderApplication.findAvailableRestaurants(5); //=> Ubiquitous Chip, Monkeyz
-console.log("\nFind restaurants serving salad: ");
-restaurantFinderApplication.findRestaurantServingDish("salad"); //=> Paesano, Ubiquitous Chip
-console.log("\nNumber of restaurants in city centre: ");
-restaurantFinderApplication.countNumberOfRestaurantsInArea("center"); //=> 2
-
 /* ---------------------------------- Tests --------------------------------- */
-//! I couldn't pass them :((
-/* const restaurantsAvailableFor5People = restaurantFinderApplication.findAvailableRestaurants(5);
-console.log(`Find available restaurants for 5 people: Expected result: Ubiquitous Chip,Monkeyz, actual result: ${restaurantsAvailableFor5People}`);
+
+const restaurantsAvailableFor5People = restaurantFinderApplication.findAvailableRestaurants(5);
+console.log(
+	`Find available restaurants for 5 people: Expected result: Ubiquitous Chip,Monkeyz, actual result: ${restaurantsAvailableFor5People}`
+);
 
 const restaurantsServingSalad = restaurantFinderApplication.findRestaurantServingDish("salad");
-console.log(`Find restaurants serving salad: Expected result: Paesano,Ubiquitous Chip, actual result: ${restaurantsServingSalad}`);
+console.log(
+	`Find restaurants serving salad: Expected result: Paesano,Ubiquitous Chip, actual result: ${restaurantsServingSalad}`
+);
 
 const numberOfRestaurantsInCityCentre = restaurantFinderApplication.countNumberOfRestaurantsInArea("center");
-console.log(`Number of restaurants in city centre: Expected result: 2, actual result: ${numberOfRestaurantsInCityCentre}`); */
+console.log(
+	`Number of restaurants in city centre: Expected result: 2, actual result: ${numberOfRestaurantsInCityCentre}`
+);
