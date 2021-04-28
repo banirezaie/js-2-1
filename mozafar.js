2b67ecd1d691c8167db76483f76a428ddc1d189a
zahra-moeini <z.sh.moeini1379@gmail.com>
2021-04-29 00:08:04 +0430

    branch-zahramoeini2

--- mozafar.js
+++ mozafar.js
@@ -23,4 +23,6 @@ let mentorsNamedUppercased = mentorsNames.map((mentorName) =>mentorName.toUpperC
 console.log(mentorsNamedUppercased);
 // prints [ 'JAMES', 'JOSH', 'JAMIE', 'MOZAFAR' ]
 
-
+// 
+// 
+// 
