5ee04289f06cd2e9217963955503fd11be8a8e4b
aydaBastami <aydabstmi@gmail.com>
2021-04-28 15:45:22 -0700

    3

--- trainee.js
+++ trainee.js
@@ -3,7 +3,10 @@ const traineeGrades = {
   george: 17,
   abdul: 19,
 }
-
+delete traineeGrades.george;
+for(const names in traineeGrades){
+  console.log(`${names.toUpperCase()} - ${traineeGrades[names]}`);
+}
 // Prints
 // TOM - 20
 // ABDUL - 19
