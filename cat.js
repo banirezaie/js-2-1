ee108d9cd2dcfbc35e05fe7100b909bfb0065c98
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-04-28 19:05:51 +0430

    this leaned

--- cat.js
+++ cat.js
@@ -1,19 +1,11 @@
-//DON'T PANIC IF IT'S HARD TO REMEMBER, YOU CAN COME BACK HERE
-//MAKE SURE OPEN IT IN A BROWSER WINDOW, YOU CAN RUN index.html
-//window
-
 const cat = {
-  name: "cat",
-  color: "grey",
-  breed: "persian cat",
-  meow() {
-    console.log(`${this.color} ${this.breed} cats are amazing`)
-  },
-  thisIs: `this is ${this}`,
-}
+	name: "cat",
+	color: "grey",
+	breed: "persian cat",
+	meow() {
+		return `${this.color} ${this.breed} cats are amazing`;
+	},
+	thisIs: `this is ${this}`,
+};
 
-console.log(cat.meow())
-
-const meow2 = cat.meow
-
-console.log(cat.meow2)
+console.log(cat.meow()); // grey persian cat cats are amazing
